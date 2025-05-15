a1ac52c34edd9c1415c2309d1c44fc4dc5202a19
kumarkapil3012 <kapilofficial4@gmail.com>
2025-05-15 13:05:49 +0530

    Calc base address of Struct form any one member of struct and paading

--- Storage_classes/find_base_address_struct.c
+++ Storage_classes/find_base_address_struct.c
@@ -2,15 +2,21 @@
 
 #define CONTAINER(ptr,type,member) ((type*)((char*)ptr-(char*)&(((type*)0)->member)))
 
+//#pragma pack(1)   -> remove padding
+
 struct MyStruct {
      int member1;
      char member2;
      double member3;
-};
+};                      // __attribute__((packed));  -> add this to remove padding
 
 int main() {
 	struct MyStruct myStruct;
 	struct MyStruct *base_address;
+
+	printf("Size With Padding : %lu\n", sizeof(myStruct));
+	//printf("Size Without Padding : %lu\n", sizeof(myStruct));
+
 	
 	int *member1_address = &(myStruct.member1);
 	//Calculate the offset from the beginning of the structure
