f4d48fa379bce65e5d618fbd733d5fc106da0374
kumarkapil3012 <kapilofficial4@gmail.com>
2025-05-15 16:58:25 +0530

    Added Array of pointers and pointer to array

--- Arrays_Pointers/pointerToPointer.c
+++ Arrays_Pointers/pointerToPointer.c
@@ -18,6 +18,19 @@ int main() {
 	printf("Address of r %p : %p\n", &r, *s);
 	printf("Address of s %p\n", &s);
 
+	int arr[2][4] = {{1,2,3,4},
+			{5,6,7,8}};
+	int *ptr[5] ; // Array of pointers -> (stores addresses of integer type) -> ptr is a array of integer pointers whose size is 5
+	int (*ptr1)[4] ; // pointer to array -> (store address of integer array) -> ptr1 is a pointer to array of intergers whose size is 4
+	
+	int *pt = (int *)arr;
+	ptr1 = arr;
+
+	
+	printf("Value of pt : %d : %d\n",*pt, *(pt+1));  // print value 1,2
+	printf("Value of ptr1 : %d : %d : %d\n",**ptr1,*((*ptr1)+1),**(ptr1+1));  // print value 1,2,5
+							 
+	
 	return 0;
 }
 
