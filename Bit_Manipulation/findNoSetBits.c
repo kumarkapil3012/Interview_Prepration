3c368c458fd339b263b6e316d28a6c94ec30cec1
kumarkapil3012 <kapilofficial4@gmail.com>
2025-05-16 14:39:06 +0530

    Calculate number of set/reset bits

--- Bit_Manipulation/findNoSetBits.c
+++ Bit_Manipulation/findNoSetBits.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 
 int main() {
-	int num,set_count = 0, reset_count = 0;
+	int num,temp,count = 0,set_count = 0, reset_count = 0;
 
 	printf("Enter Number : ");
 	scanf("%d",&num);
-
+	temp = num;
 	while(num != 0) {
 		if((num & 1) == 1) 
 			set_count++;
-		reset_count++;
+		else 
+			reset_count++;
 		num >>= 1;
 	}
+	//diff method
+	while(temp != 0) {
+		temp &= (temp-1);
+		count++;
+	}
 
+	printf("Number of Set bits : %d\n",count);
 	printf("Number of Set bits : %d\n",set_count);
 	printf("Number of Reset bits : %d\n",reset_count);
 
