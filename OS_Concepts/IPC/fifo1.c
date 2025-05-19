d48962f95f72cfa3eb3f587b461df4f7e734103d
kumarkapil3012 <kapilofficial4@gmail.com>
2025-05-19 15:32:56 +0530

    Named Pipe/mkfifo

---
+++ OS_Concepts/IPC/fifo1.c
@@ -0,0 +1,32 @@
+#include "my_header.h"
+
+int main() {
+	int fd, fd1;
+	char a[20];
+
+	mkfifo("f1", 0666);
+	perror("mkfifo");
+	mkfifo("f2",0666);
+	perror("mkfifo");
+
+	fd =  open("f1", O_RDWR);
+	fd1 =  open("f2", O_RDWR);
+	
+	printf("PID = %d\n",getpid());
+
+	if(fork() == 0) {
+		while(1) {
+			printf("Enter the data :\n");
+			scanf("%s",a);
+			write(fd, a, strlen(a)+1);
+		}
+	}
+	else {
+		while(1) {	
+			read(fd1, a, sizeof(a));
+			printf("Data : %s\n", a);
+		}
+	}
+	return 0;
+}
+
