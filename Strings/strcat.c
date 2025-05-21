2287f50eb9920fdf7af1adfa79098c8c951ed477
kumarkapil3012 <kapilofficial4@gmail.com>
2025-05-21 12:44:04 +0530

    String Concatinate Functions (strcat,strncat,strlcat)

--- Strings/strcat.c
+++ Strings/strcat.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdio_ext.h>
+#include <string.h>
 
 char *my_strcat(char *dest, char *src) {
 	int i=0, j=0;
@@ -33,8 +34,21 @@ char *my_strncat(char *dest, char *src, int n) {
 	return dest;
 }
 
+int my_strlcat(char *dest, const char *src, int size) {
+	int dest_len = strlen(dest);
+	int src_len = strlen(src);
+	if (size <= dest_len) {
+		return size + src_len;
+	}
+	if (size > dest_len) {
+		strncpy(dest + dest_len, src, size - dest_len - 1);
+		dest[size - 1] = '\0';
+	}
+	return dest_len + src_len;
+}
+
 int main() {
-	char src[10], dest[20];
+	char src[10], dest[50];
 	int n;
 	printf("Enter the Src String : ");
 	scanf("%[^\n]s",src);
@@ -47,9 +61,11 @@ int main() {
 
 	char *output = my_strcat(dest, src);
 	char *output1 = my_strncat(dest, src, n);
+	int output2 = my_strlcat(dest, src, n);
 	
 	printf("Final String strcat() : %s\n",output);
 	printf("Final String strncat() : %s\n",output1);
+	printf("Final String strlcat() : %d\n",output2);
 
 	return 0;
 }
