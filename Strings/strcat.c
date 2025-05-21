336b8c86459b8e0ffe367e60a2c35b95f990a2f2
kumarkapil3012 <kapilofficial4@gmail.com>
2025-05-21 10:29:55 +0530

    Adding strncat() function

--- Strings/strcat.c
+++ Strings/strcat.c
@@ -17,19 +17,39 @@ char *my_strcat(char *dest, char *src) {
 	return dest;
 }
 
+char *my_strncat(char *dest, char *src, int n) {
+	int i=0, j=0;
+	if(src == NULL || dest == NULL) {
+		return  dest;
+	}
+	while(dest[i] != '\0') {
+		i++;
+	}
+	while(src[j] != '\0' && n > 0) {
+		dest[i] = src[j];
+		j++, i++, n--;
+	}
+	dest[i+1] = '\0';
+	return dest;
+}
 
 int main() {
 	char src[10], dest[20];
+	int n;
 	printf("Enter the Src String : ");
 	scanf("%[^\n]s",src);
 	__fpurge(stdin);
 	printf("Enter the Dest String : ");
 	scanf("%[^\n]s",dest);
 	__fpurge(stdin);
+	printf("Enter the No : ");
+	scanf("%d",&n);
 
 	char *output = my_strcat(dest, src);
-
-	printf("Final String : %s\n",output);
+	char *output1 = my_strncat(dest, src, n);
+	
+	printf("Final String strcat() : %s\n",output);
+	printf("Final String strncat() : %s\n",output1);
 
 	return 0;
 }
